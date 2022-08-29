378a0cb583c826b43b07a49710b775a102b02f4d
严雄 <yanxiong@yupaowang.com>
2022-08-29 11:37:10 +0800

    增加部分参数

---
+++ module_compose/src/main/java/com/mvvm/module_compose/ComPoseActivity.kt
@@ -0,0 +1,20 @@
+package com.mvvm.module_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+
+/**
+ * Created by yan_x
+ * @date 2022/6/29/029 14:05
+ * @description
+ */
+class ComPoseActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
