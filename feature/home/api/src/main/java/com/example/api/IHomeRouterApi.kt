2cbcb74d05330139e632be0dede1124ba9fcdcf9
严雄 <inameyx@gmail.com>
2024-08-14 13:38:38 +0800

    mod 移除多余引用

---
+++ feature/home/api/src/main/java/com/example/api/IHomeRouterApi.kt
@@ -0,0 +1,13 @@
+package com.example.api
+
+import com.alibaba.android.arouter.facade.template.IProvider
+
+/**
+
+ * <p>创建时间：2024/8/14/014</p>
+ *
+ * @author yanxiong
+ */
+interface  IHomeRouterApi:IProvider {
+    fun startTest()
+}
